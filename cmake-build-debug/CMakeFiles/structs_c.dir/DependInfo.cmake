4199dca6dafa2abb73d0308b8d292bbb87256066
oudream <oudream@126.com>
2019-05-08 19:48:16 +0800

    provide cmake support
    provide cmake support

---
+++ cmake-build-debug/CMakeFiles/structs_c.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/ddd/algorithm/struct-c/c/aatree.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/aatree.c.o"
+  "/ddd/algorithm/struct-c/c/avltree.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/avltree.c.o"
+  "/ddd/algorithm/struct-c/c/binheap.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/binheap.c.o"
+  "/ddd/algorithm/struct-c/c/binomial.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/binomial.c.o"
+  "/ddd/algorithm/struct-c/c/cursor.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/cursor.c.o"
+  "/ddd/algorithm/struct-c/c/disjsets.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/disjsets.c.o"
+  "/ddd/algorithm/struct-c/c/dsl.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/dsl.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_38.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_38.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_40.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_40.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_43.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_43.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_45.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_45.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_46.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_46.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_53.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_53.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_54.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_54.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_55.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_55.c.o"
+  "/ddd/algorithm/struct-c/c/fig10_62.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig10_62.c.o"
+  "/ddd/algorithm/struct-c/c/fig1_2.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig1_2.c.o"
+  "/ddd/algorithm/struct-c/c/fig1_3.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig1_3.c.o"
+  "/ddd/algorithm/struct-c/c/fig1_4.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig1_4.c.o"
+  "/ddd/algorithm/struct-c/c/fig2_10.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig2_10.c.o"
+  "/ddd/algorithm/struct-c/c/fig2_11.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig2_11.c.o"
+  "/ddd/algorithm/struct-c/c/fig2_9.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/fig2_9.c.o"
+  "/ddd/algorithm/struct-c/c/hashfunc.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/hashfunc.c.o"
+  "/ddd/algorithm/struct-c/c/hashquad.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/hashquad.c.o"
+  "/ddd/algorithm/struct-c/c/hashsep.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/hashsep.c.o"
+  "/ddd/algorithm/struct-c/c/kdtree.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/kdtree.c.o"
+  "/ddd/algorithm/struct-c/c/leftheap.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/leftheap.c.o"
+  "/ddd/algorithm/struct-c/c/list.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/list.c.o"
+  "/ddd/algorithm/struct-c/c/max_sum.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/max_sum.c.o"
+  "/ddd/algorithm/struct-c/c/pairheap.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/pairheap.c.o"
+  "/ddd/algorithm/struct-c/c/poly.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/poly.c.o"
+  "/ddd/algorithm/struct-c/c/queue.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/queue.c.o"
+  "/ddd/algorithm/struct-c/c/redblack.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/redblack.c.o"
+  "/ddd/algorithm/struct-c/c/sort.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/sort.c.o"
+  "/ddd/algorithm/struct-c/c/splay.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/splay.c.o"
+  "/ddd/algorithm/struct-c/c/stackar.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/stackar.c.o"
+  "/ddd/algorithm/struct-c/c/stackli.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/stackli.c.o"
+  "/ddd/algorithm/struct-c/c/testaa.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testaa.c.o"
+  "/ddd/algorithm/struct-c/c/testavl.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testavl.c.o"
+  "/ddd/algorithm/struct-c/c/testbin.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testbin.c.o"
+  "/ddd/algorithm/struct-c/c/testcurs.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testcurs.c.o"
+  "/ddd/algorithm/struct-c/c/testdsl.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testdsl.c.o"
+  "/ddd/algorithm/struct-c/c/testhash.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testhash.c.o"
+  "/ddd/algorithm/struct-c/c/testheap.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testheap.c.o"
+  "/ddd/algorithm/struct-c/c/testleft.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testleft.c.o"
+  "/ddd/algorithm/struct-c/c/testlist.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testlist.c.o"
+  "/ddd/algorithm/struct-c/c/testpair.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testpair.c.o"
+  "/ddd/algorithm/struct-c/c/testque.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testque.c.o"
+  "/ddd/algorithm/struct-c/c/testrb.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testrb.c.o"
+  "/ddd/algorithm/struct-c/c/testsply.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testsply.c.o"
+  "/ddd/algorithm/struct-c/c/teststka.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/teststka.c.o"
+  "/ddd/algorithm/struct-c/c/teststkl.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/teststkl.c.o"
+  "/ddd/algorithm/struct-c/c/testtree.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testtree.c.o"
+  "/ddd/algorithm/struct-c/c/testtrp.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/testtrp.c.o"
+  "/ddd/algorithm/struct-c/c/treap.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/treap.c.o"
+  "/ddd/algorithm/struct-c/c/tree.c" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_c.dir/c/tree.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/structs_cpp.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/ddd/algorithm/struct-c/cpp/BuggyIntCell.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/BuggyIntCell.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/CaseInsensitiveHashTable.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/CaseInsensitiveHashTable.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/CuckooHashTable.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/CuckooHashTable.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/DisjSets.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/DisjSets.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_02.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_02.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_03.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_03.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_04.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_04.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_05.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_05.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_06.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_06.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_10.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_10.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_11.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_11.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_18.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_18.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_21.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_21.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_23.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_23.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_24.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_24.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig01_25.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig01_25.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig02_09.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig02_09.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig02_10.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig02_10.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig02_11.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig02_11.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_38.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_38.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_40.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_40.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_43.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_43.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_45.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_45.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_46.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_46.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_53.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_53.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Fig10_63.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Fig10_63.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/FindMax.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/FindMax.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/IntCell.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/IntCell.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/KdTree.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/KdTree.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/LambdaExample.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/LambdaExample.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/MaxSumTest.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/MaxSumTest.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/MemoryCell.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/MemoryCell.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/MemoryCellExpand.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/MemoryCellExpand.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/QuadraticProbing.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/QuadraticProbing.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/RadixSort.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/RadixSort.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/Random.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/Random.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/RemoveEveryOtherItem.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/RemoveEveryOtherItem.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/SeparateChaining.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/SeparateChaining.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/SuffixArray.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/SuffixArray.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TailRecursion.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TailRecursion.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestAvlTree.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestAvlTree.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestBinaryHeap.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestBinaryHeap.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestBinarySearchTree.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestBinarySearchTree.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestBinomialQueue.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestBinomialQueue.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestCuckooHashTable.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestCuckooHashTable.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestFastDisjSets.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestFastDisjSets.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestIntCell.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestIntCell.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestLeftistHeap.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestLeftistHeap.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestList.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestList.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestMemoryCell.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestMemoryCell.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestPQ.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestPQ.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestPairingHeap.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestPairingHeap.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestQuadraticProbing.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestQuadraticProbing.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestRandom.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestRandom.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestRedBlackTree.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestRedBlackTree.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestSeparateChaining.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestSeparateChaining.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestSlowDisjSets.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestSlowDisjSets.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestSort.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestSort.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestSplayTree.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestSplayTree.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestTreap.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestTreap.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/TestVector.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/TestVector.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/WordLadder.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/WordLadder.cpp.o"
+  "/ddd/algorithm/struct-c/cpp/mapDemo.cpp" "/ddd/algorithm/struct-c/cmake-build-debug/CMakeFiles/structs_cpp.dir/cpp/mapDemo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
